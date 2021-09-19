7c6fd18827e82bbb8418b40eb8152ce8da1816a4
lsalgado <lsalgado@muxi.com.br>
2021-09-19 18:12:58 -0300

    Removing old elections after getting the new ones from the api.

--- app/src/main/java/com/example/android/politicalpreparedness/repository/ElectionRepository.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/repository/ElectionRepository.kt
@@ -2,7 +2,6 @@ package com.example.android.politicalpreparedness.repository
 
 import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.example.android.politicalpreparedness.BuildConfig
 import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.network.CivicsApi
@@ -15,14 +14,34 @@ class ElectionRepository(val context: Context) {
 
     val database: ElectionDatabase by lazy { ElectionDatabase.getInstance(context)}
 
-    val elections: LiveData<List<Election>> = database.electionDao.getElections()
+    val currentElections: LiveData<List<Election>> = database.electionDao.getCurrentElections()
+    val savedElections: LiveData<List<Election>> = database.electionDao.getSavedElections()
 
     suspend fun refreshElections() {
         withContext(Dispatchers.IO) {
             val response = CivicsApi.retrofitService.getElections(BuildConfig.API_KEY)
-            Timber.e("batata")
             Timber.e(response.toString())
-            database.electionDao.insertAll(*response.elections.toTypedArray())
+            val newElections = response.elections.toTypedArray()
+            deleteOldNonSavedElections(newElections)
+            database.electionDao.insertAll(*newElections)
+        }
+    }
+
+    /**
+     * Database maintenance method used to remove all old elections that are not saved.
+     */
+    private suspend fun deleteOldNonSavedElections(elections: Array<Election>) {
+        withContext(Dispatchers.IO) {
+            elections.forEach { election ->
+                if (!elections.contains(election)) {
+                    if (!election.saved) {
+                        database.electionDao.deleteElection(election.id)
+                    } else if (!election.old){
+                        election.old = true
+                        database.electionDao.update(election)
+                    }
+                }
+            }
         }
     }
 }
