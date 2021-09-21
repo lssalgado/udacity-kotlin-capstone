decaa37a3cf12dbf5ce0e18638021e9521b27976
lsalgado <lsalgado@muxi.com.br>
2021-09-21 16:03:55 -0300

    Implementing VoterInfo fragment, viewmodel and layout.

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -1,8 +1,12 @@
 package com.example.android.politicalpreparedness.election
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
 
@@ -10,6 +14,7 @@ class VoterInfoFragment : Fragment() {
 
     private lateinit var viewModel: VoterInfoViewModel
     private lateinit var binding: FragmentVoterInfoBinding
+    private lateinit var toast: Toast
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -23,7 +28,21 @@ class VoterInfoFragment : Fragment() {
         viewModel =
             ViewModelProvider(this, viewModelFactory).get(VoterInfoViewModel::class.java)
 
+        binding.viewModel = viewModel
 
+        viewModel.urlToLoad.observe(viewLifecycleOwner, Observer { url ->
+            url?.let {
+                startUrlIntent(it)
+                viewModel.onUrlIntentStarted()
+            }
+        })
+
+        viewModel.toastText.observe(viewLifecycleOwner, Observer { id ->
+            id?.let {
+                showToast(it)
+                viewModel.onToastShown()
+            }
+        })
 
         //TODO: Add binding values
 
@@ -41,5 +60,18 @@ class VoterInfoFragment : Fragment() {
     }
 
     //TODO: Create method to load URL intents
+    private fun startUrlIntent(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+
+    private fun showToast(id: Int) {
+        if (::toast.isInitialized) {
+            // Cancels the current toast to avoid queueing multiple toasts
+            toast.cancel()
+        }
+        toast = Toast.makeText(context, id, Toast.LENGTH_SHORT)
+        toast.show()
+    }
 
 }
--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -1,19 +1,55 @@
 package com.example.android.politicalpreparedness.election
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.android.politicalpreparedness.BuildConfig
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.network.CivicsApi
 import com.example.android.politicalpreparedness.network.models.Division
+import com.example.android.politicalpreparedness.network.models.Election
+import com.example.android.politicalpreparedness.network.models.State
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.example.android.politicalpreparedness.repository.ElectionRepository
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class VoterInfoViewModel(
     private val repository: ElectionRepository,
-    electionId: Int,
-    division: Division
+    private val electionId: Int,
+    val division: Division
 ) : ViewModel() {
+
+    private val _voterInfo = MutableLiveData<VoterInfoResponse>()
+    val voterInfo: LiveData<VoterInfoResponse>
+        get() = _voterInfo
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
+
+    val election = MediatorLiveData<Election>()
 
     init {
         Timber.e("ElectionId = $electionId")
         Timber.e("Division = $division")
+        getVoterInfo()
+        election.addSource(repository.getElectionById(electionId), election::setValue)
+    }
+
+    private fun getVoterInfo() {
+        viewModelScope.launch {
+            val voterInfo = CivicsApi.retrofitService.getVoterInfo(
+                "${division.country}, ${division.state}",
+                electionId,
+                BuildConfig.API_KEY
+            )
+            Timber.e(voterInfo.toString())
+            _voterInfo.value = voterInfo
+            if (voterInfo.state != null && voterInfo.state.isNotEmpty()){
+                _state.value = voterInfo.state[0]
+            } else {
+                _toastText.value = R.string.missing_state
+            }
+        }
     }
 
     //TODO: Add live data to hold voter info
@@ -21,12 +57,49 @@ class VoterInfoViewModel(
     //TODO: Add var and methods to populate voter info
 
     //TODO: Add var and methods to support loading URLs
+    private val _urlToLoad = MutableLiveData<String>()
+    val urlToLoad: LiveData<String>
+        get() = _urlToLoad
 
-    //TODO: Add var and methods to save and remove elections to local database
-    //TODO: cont'd -- Populate initial state of save button to reflect proper action based on election saved status
+    private val _toastText = MutableLiveData<Int>()
+    val toastText: LiveData<Int>
+        get() = _toastText
 
-    /**
-     * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
-     */
+    fun onUrlClick(url: String) {
+        if (url.isNotEmpty()) {
+            _urlToLoad.value = url
+        } else {
+            _toastText.value = R.string.missing_url
+        }
+    }
 
+    fun onUrlIntentStarted() {
+        _urlToLoad.value = null
+    }
+
+    fun onToastShown() {
+        _toastText.value = null
+    }
+
+    fun onFollowClick() {
+        viewModelScope.launch {
+            val currentElection = election.value
+            if (currentElection != null) {
+                val saved = currentElection.saved
+                val old = currentElection.old
+                // Is currently saved and is a past election
+                if (old && saved) {
+                    // Them it should be removed from the database
+                    repository.deleteElection(currentElection.id)
+                } else {
+                    // Otherwise, just invert the current `saved` value
+                    currentElection.saved = !saved
+                    repository.updateElection(currentElection)
+                }
+            } else {
+                _toastText.value = R.string.missing_election
+            }
+        }
+
+    }
 }
